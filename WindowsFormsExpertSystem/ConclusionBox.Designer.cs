ba63ac091bda3ff5ee5946506084947010727713
AdamStan <adam15stan@gmail.com>
2018-07-16 21:54:46 +0200

    The last commit, I hope

--- WindowsFormsExpertSystem/ConclusionBox.Designer.cs
+++ WindowsFormsExpertSystem/ConclusionBox.Designer.cs
@@ -35,7 +35,7 @@
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(175, 86);
+            this.button1.Location = new System.Drawing.Point(177, 86);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(75, 23);
             this.button1.TabIndex = 0;
@@ -46,16 +46,18 @@
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(226, 34);
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.label1.ForeColor = System.Drawing.Color.Red;
+            this.label1.Location = new System.Drawing.Point(35, 51);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(35, 13);
+            this.label1.Size = new System.Drawing.Size(51, 20);
             this.label1.TabIndex = 1;
             this.label1.Text = "label1";
             // 
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(12, 34);
+            this.label2.Location = new System.Drawing.Point(35, 24);
             this.label2.Name = "label2";
             this.label2.Size = new System.Drawing.Size(217, 13);
             this.label2.TabIndex = 2;
--- WindowsFormsExpertSystem/ConclusionModule.cs
+++ WindowsFormsExpertSystem/ConclusionModule.cs
@@ -5,6 +5,7 @@ using System.Data.SqlClient;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Forms;
 
 namespace WindowsFormsExpertSystem
 {
@@ -24,61 +25,69 @@ namespace WindowsFormsExpertSystem
              * inner join FactsToConclusions ftc on f.Id = ftc.fact_id
              * inner join Conclusions c on c.Id = ftc.conclusion_id
              */
-            DataTable ftc = new DataTable();
-            DataTable c = new DataTable();
-
-            SqlDataAdapter adp1 = new SqlDataAdapter("select * from FactsToConclusions", 
-                this.behind.conn_MsSQL);
-            SqlDataAdapter adp2 = new SqlDataAdapter("select * from Conclusions", 
-                this.behind.conn_MsSQL);
-
-            adp1.Fill(ftc);
-            adp2.Fill(c);
-
-            var join_tables = from fact in MainMenuWindow.tableFacts.AsEnumerable()
-                              join fact_to_c in ftc.AsEnumerable() on fact["Id"] equals fact_to_c["fact_id"]
-                              join conc in c.AsEnumerable() on fact_to_c["conclusion_id"] equals conc["Id"]
-                              select new
-                              {
-                                  fact_id = (int)fact_to_c["fact_id"],
-                                  conclusion_id = (int)fact_to_c["conclusion_id"],
-                                  conclusion_name = (string)conc["conclusion_name"],
-                                  value_should_be = (bool)fact_to_c["val"],
-                                  value_is = (bool)fact["is_true"]
-                              };
-
-            /* Teraz zaczyna sie wnioskowanie */
-            string konkluzja = "smierc naturalna";
-
-            foreach (DataRow row in c.Rows)
+            try
             {
-                var getOnlyMyFacts = from fact in join_tables
-                                     where fact.conclusion_id == (int)row["Id"]
-                                     select fact;
-                bool flag = false;
-                foreach (var item in getOnlyMyFacts)
+                DataTable ftc = new DataTable();
+                DataTable c = new DataTable();
+
+                SqlDataAdapter adp1 = new SqlDataAdapter("select * from FactsToConclusions",
+                    this.behind.conn_MsSQL);
+                SqlDataAdapter adp2 = new SqlDataAdapter("select * from Conclusions",
+                    this.behind.conn_MsSQL);
+
+                adp1.Fill(ftc);
+                adp2.Fill(c);
+
+                var join_tables = from fact in MainMenuWindow.tableFacts.AsEnumerable()
+                                  join fact_to_c in ftc.AsEnumerable() on fact["Id"] equals fact_to_c["fact_id"]
+                                  join conc in c.AsEnumerable() on fact_to_c["conclusion_id"] equals conc["Id"]
+                                  select new
+                                  {
+                                      fact_id = (int)fact_to_c["fact_id"],
+                                      conclusion_id = (int)fact_to_c["conclusion_id"],
+                                      conclusion_name = (string)conc["conclusion_name"],
+                                      value_should_be = (bool)fact_to_c["val"],
+                                      value_is = (bool)fact["is_true"],
+                                      desc = (string)conc["description"]
+                                  };
+
+                /* Teraz zaczyna sie wnioskowanie */
+                string konkluzja = "Smierc naturalna";
+
+                foreach (DataRow row in c.Rows)
                 {
-                    Console.WriteLine("--> Nazwa konkluzji: " + item.conclusion_name);
-                    Console.WriteLine(item.value_is + " " + item.value_should_be);
-                    if(item.value_is == item.value_should_be)
+                    var getOnlyMyFacts = from fact in join_tables
+                                         where fact.conclusion_id == (int)row["Id"]
+                                         select fact;
+                    bool flag = false;
+                    foreach (var item in getOnlyMyFacts)
                     {
-                        flag = true;
+                        Console.WriteLine("--> Nazwa konkluzji: " + item.conclusion_name);
+                        Console.WriteLine(item.value_is + " " + item.value_should_be);
+                        if (item.value_is == item.value_should_be)
+                        {
+                            flag = true;
+                        }
+                        else
+                        {
+                            flag = false;
+                            break;
+                        }
                     }
-                    else
+                    if (flag)
                     {
-                        flag = false;
+                        konkluzja = (string)row["description"];
                         break;
                     }
                 }
-                if (flag)
-                {
-                    konkluzja = (string)row["conclusion_name"];
-                    break;
-                }
+                Console.WriteLine(konkluzja);
+                new ConclusionBox(konkluzja).Show();
+                /*dla kazdej przezslanki sprawdzamy czy fakty maja wartosci konieczne*/
             }
-            Console.WriteLine(konkluzja);
-            new ConclusionBox(konkluzja).Show();
-            /*dla kazdej przezslanki sprawdzamy czy fakty maja wartosci konieczne*/
+            catch (Exception ex)
+            {
+                MessageBox.Show("Na niektóre pytania nie udzielono odpowiedzi");
+            }
         }
     }
 }
